3b20ae5f1ddfd3a87e59fbab7ae5bdc301d475fb
Siarhei Beliakou <sbeliakou@gmail.com>
2018-10-15 23:28:25 +0300

    vm ip

--- monitoring_prometheus/prometheus.sh
+++ monitoring_prometheus/prometheus.sh
@@ -1,17 +1,16 @@
 #!/bin/bash
 
-yum install -y git
+yum install -y git 
 
 cd /tmp
 git clone https://github.com/shreben/prometheus.git
 cd prometheus/
-docker-compose up -d
-
+docker-compose up -d && \
 cat <<EOF
-http://localhost:3000           grafana (admin/foobar)
-http://localhost:9090           prometheus
-http://localhost:9093           alertmanager
-http://localhost:8500/ui/       consul
-http://localhost:8080			cadvisor
-http://localhost:9323/metrics   full set of metrics exposed by docker engine
+http://$(hostname -I | awk '{print $2}'):3000           grafana (admin/foobar)
+http://$(hostname -I | awk '{print $2}'):9090           prometheus
+http://$(hostname -I | awk '{print $2}'):9093           alertmanager
+http://$(hostname -I | awk '{print $2}'):8500/ui/       consul
+http://$(hostname -I | awk '{print $2}'):8080			cadvisor
+http://$(hostname -I | awk '{print $2}'):9323/metrics   full set of metrics exposed by docker engine
 EOF
