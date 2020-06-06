7082f5e3e994310dc5b3d3960d02534dcb91f357
Siarhei Beliakou <sbeliakou@gmail.com>
2020-06-06 21:03:44 +0300

    updated

--- install/ubuntu.sh
+++ install/ubuntu.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 apt-get update
 apt-get install -y \
     apt-transport-https \
@@ -15,8 +17,8 @@ add-apt-repository \
    stable"
 
 apt-get update
-apt-get install -y docker-ce docker-ce-cli containerd.io
-apt-get install -y --allow-downgrades docker-ce-cli=5:19.03.8~3-0~ubuntu-bionic
+apt-get install -y docker-ce
+systemctl enable --now docker
 
-curl -L "https://github.com/docker/compose/releases/download/1.25.5/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+curl -L "https://github.com/docker/compose/releases/download/1.26.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 chmod a+x /usr/local/bin/docker-compose
